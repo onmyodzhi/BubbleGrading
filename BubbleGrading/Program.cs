9af8044af9502a1ac61eda1910d1c7864cc78f2d
R3ndyyyy <mrkobalt18@gmail.com>
2023-02-25 23:30:55 +0300

    Implemented bubble sorting from lesser to greater and from greater to lesser

---
+++ BubbleGrading/Program.cs
@@ -0,0 +1,55 @@
+﻿int[] CreateArray(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 10);
+    }
+    return array;
+}
+int[] SortTheArrayFromSmallestToLargest(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = 0; j < array.Length - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+int[] SortTheArrayFromLargerToSmaller(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = 0; j < array.Length - 1 - i; j++)
+        {
+            if (array[j] < array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine();
+}
+int[] array = CreateArray(10);
+PrintArray(array);
+PrintArray(SortTheArrayFromSmallestToLargest(array));
+PrintArray(SortTheArrayFromLargerToSmaller(array));
